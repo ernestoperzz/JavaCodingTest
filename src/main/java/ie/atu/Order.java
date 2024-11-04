9fcfea71de1e20e54cf9827df2bda0b72bacd75c
ERNESTOALONSOPEREZ-S <g00436110@atu.ie>
2024-11-04 10:30:02 +0000

    'New files added'

---
+++ src/main/java/ie/atu/Order.java
@@ -0,0 +1,50 @@
+package ie.atu;
+
+import java.util.ArrayList;
+
+public class Order {
+private string orderld;
+private string customerName;
+private string productName;
+
+    public void setOrderld(string customerName, string productName) {
+        customerName = customer;
+        productName = product;
+    }
+
+    public string getOrderld(){
+        return customerName;
+        return productName;
+    }
+
+
+    public static void main(String[] args) {
+       int quantity;
+        string orderld;
+        string customerName;
+        string productName;
+
+
+//define arraylist
+        ArrayList<String> orders = new ArrayList<String>();
+//add order to arraylist
+        orders.add("Paolo, Milk, x2");
+        orders.add("Leo, Still water, x1");
+        orders.add("Paolo, Oreo cookies, x4");
+        orders.add("Paolo, Ice cream, x1");
+        orders.add("Paolo, Coconut, x2");
+
+//print tasks using enhanced for loop
+System.out.println("Order list:");
+for(int i = 0; i< orders.size(); i++){
+    System.out.println((i+1)+". "+ tasks.get(i));
+}
+//update arraylist
+
+
+    }
+
+
+
+}
+
---
+++ src/main/java/ie/atu/main.java
@@ -0,0 +1,24 @@
+package ie.atu;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("1.Add order");
+        System.out.println("2.Update order");
+        System.out.println("3.Display all orders");
+        System.out.println("4.Exit");
+
+        switch(menu){
+            case '1':
+
+                break;
+            case '2':
+
+                break;
+            case '3':
+
+                break;
+            case '4':
+                break;
+        }
+    }
+}
